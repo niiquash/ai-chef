7e7e023054c0b8e9c24048b8b067b0051ead75f7
niiquash <qua16006@byui.edu>
2025-01-10 23:25:16 -0700

    completed markup and css for ingredients list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,6 +13,14 @@ const Main = () => {
         />
         <button className="main--form__button">Add ingredient</button>
       </form>
+      <section className="main--ingredients">
+        <h2 className="main--ingredients__heading">Ingredients on hand:</h2>
+        <ul className="main--ingredients__list">
+          <li className="main--ingredients__list-item">main spices</li>
+          <li className="main--ingredients__list-item">rice</li>
+          <li className="main--ingredients__list-item">chicken</li>
+        </ul>
+      </section>
     </main>
   );
 };
