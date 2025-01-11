26bc84f78cf06ecc2dbf39a2fabf79db70ac6931
niiquash <qua16006@byui.edu>
2025-01-11 01:02:52 -0700

    refactored: form to grab formdata and populate state variable. created new components.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import ApiRecipe from "./ApiRecipe";
 
 const Main = () => {
+  const [ingredients, setIngredients] = React.useState([]);
+  const [showRecipe, setShowRecipe] = React.useState(false);
+
+  const getIngredients = (formData) => {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevList) => [...prevList, newIngredient]);
+  };
+
+  const toggleShowRecipe = () => {
+    setShowRecipe((prev) => !prev);
+  };
+
+  const ingredientsList = ingredients.map((ingredient) => (
+    <li key={ingredient} className="main--ingredients__list-item">
+      {ingredient}
+    </li>
+  ));
+
   return (
     <main className="main">
-      <form className="main--form">
+      <form action={getIngredients} className="main--form">
         <input
           type="text"
           name="ingredient"
@@ -15,41 +34,25 @@ const Main = () => {
       </form>
       <section className="main--ingredients">
         <h2 className="main--ingredients__heading">Ingredients on hand:</h2>
-        <ul className="main--ingredients__list">
-          <li className="main--ingredients__list-item">Chicken breasts</li>
-          <li className="main--ingredients__list-item">
-            Most of the main spices
-          </li>
-          <li className="main--ingredients__list-item">Olive oil</li>
-          <li className="main--ingredients__list-item">Heavy Cream</li>
-          <li className="main--ingredients__list-item">Chicken broth</li>
-          <li className="main--ingredients__list-item">Parmesan cheese</li>
-          <li className="main--ingredients__list-item">Spinach</li>
-        </ul>
+        <ul className="main--ingredients__list">{ingredientsList}</ul>
       </section>
-      <section className="main--get-recipe">
-        <div>
-          <h3 className="main--get-recipe__heading">Ready for a recipe?</h3>
-          <p className="main--get-recipe__text">
-            Generate a recipe from your list of ingredients
-          </p>
-        </div>
-        <button className="main--get-recipe__button">Get a recipe</button>
-      </section>
-      <section className="main--suggested-recipe">
-        <h2 className="main--suggested-recipe__heading">Suggested Recipe:</h2>
-        <div className="main--suggested-recipe__card">
-          <h3 className="main--suggested-recipe__card-heading">
-            Creamy Tuscan Chicken
-          </h3>
-
-          <p className="main--suggested-recipe__card-text">
-            Creamy Tuscan Chicken is a one-pan dish that's perfect for a
-            weeknight dinner. It's made with juicy seared chicken breasts,
-            spinach, sun-dried tomatoes, and a rich and creamy sauce.
-          </p>
-        </div>
-      </section>
+      {ingredients.length > 3 && (
+        <section className="main--get-recipe">
+          <div>
+            <h3 className="main--get-recipe__heading">Ready for a recipe?</h3>
+            <p className="main--get-recipe__text">
+              Generate a recipe from your list of ingredients
+            </p>
+          </div>
+          <button
+            onClick={toggleShowRecipe}
+            className="main--get-recipe__button"
+          >
+            Get a recipe
+          </button>
+        </section>
+      )}
+      <ApiRecipe showRecipe={showRecipe} />
     </main>
   );
 };
